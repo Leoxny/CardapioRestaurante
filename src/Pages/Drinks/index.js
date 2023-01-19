aeb73a45cd9cf589e767cdd65a7bbd858a028183
Leoxny <eunao4549@gmail.com>
2023-01-19 13:01:38 -0300

    me ajuda

--- src/Pages/Drinks/index.js
+++ src/Pages/Drinks/index.js
@@ -5,13 +5,15 @@ import Header from '../../components/Header';
 
 export default function Drinks({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
+  <SafeAreaView style={styles.container}>
 
       <Header/>
 
     <ScrollView
     showsVerticalScrollIndicator={false}
     >
+    <View style={styles.boxContainer}>
+
       <View style={styles.box}>
         <View style={styles.inner}>
         <TouchableOpacity onPress={() => navigation.navigate('InformationDrinksLimon')}>
@@ -24,7 +26,8 @@ export default function Drinks({ navigation }) {
         </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+        <View style={styles.inner}>
         <TouchableOpacity onPress={() => navigation.navigate('InformationDrinksMarg')}>
           <Image
             style={styles.image}
@@ -32,39 +35,46 @@ export default function Drinks({ navigation }) {
           />
         </TouchableOpacity>
         <Text style={styles.texto}>Jasmine Margarita</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
-      <TouchableOpacity onPress={() => navigation.navigate('InformationGilmet')}>
-        <Image
-          style={styles.image}
-          source={require('../../asset/Drinks/gilmet.jpg')}
-        />
-        </TouchableOpacity>
-        <Text style={styles.texto}>Gilmet Cocktail</Text>
+      <View style={styles.box}>
+        <View style={styles.inner}>
+        <TouchableOpacity onPress={() => navigation.navigate('InformationGilmet')}>
+          <Image
+            style={styles.image}
+            source={require('../../asset/Drinks/gilmet.jpg')}
+          />
+          </TouchableOpacity>
+          <Text style={styles.texto}>Gilmet Cocktail</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
-      <TouchableOpacity onPress={() => navigation.navigate('InformationGinTonica')}>
-        <Image
-          style={styles.image}
-          source={require('../../asset/Drinks/gin.png')}
-        />
-        </TouchableOpacity>
-        <Text style={styles.texto}>Gin Tonica</Text>
+      <View style={styles.box}>
+        <View style={styles.inner}>
+        <TouchableOpacity onPress={() => navigation.navigate('InformationGinTonica')}>
+          <Image
+            style={styles.image}
+            source={require('../../asset/Drinks/gin.png')}
+          />
+          </TouchableOpacity>
+          <Text style={styles.texto}>Gin Tonica</Text>
       </View>
 
-      <View style={styles.column}>
-      <TouchableOpacity onPress={() => navigation.navigate('InformationAperol')}>
-        <Image
-          style={styles.image}
-          source={require('../../asset/Drinks/aperol.jpg')}
-        />
-        </TouchableOpacity>
-        <Text style={styles.texto}>Aperol Spritz</Text>
+      <View style={styles.box}>
+        <View style={styles.inner}>
+          <TouchableOpacity onPress={() => navigation.navigate('InformationAperol')}>
+            <Image
+              style={styles.image}
+              source={require('../../asset/Drinks/aperol.jpg')}
+            />
+            </TouchableOpacity>
+            <Text style={styles.texto}>Aperol Spritz</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationBeach')}>
         <Image
           style={styles.image}
@@ -72,9 +82,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Sex on the beach</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationTropical')}>
         <Image
           style={styles.image}
@@ -82,9 +94,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Tropical Gin</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationMojito')}>
         <Image
           style={styles.image}
@@ -92,9 +106,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Mojito</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationBoomerang')}>
         <Image
           style={styles.image}
@@ -102,9 +118,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Boomerang</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationAbacaxi')}>
         <Image
           style={styles.image}
@@ -112,9 +130,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Caipirinha de abacaxi</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationNevada')}>
         <Image
           style={styles.image}
@@ -122,9 +142,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Caipirosca nevada</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationMorango')}>
         <Image
           style={styles.image}
@@ -132,9 +154,11 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Caipirinha de morango</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
+      <View style={styles.box}>
+      <View style={styles.inner}>
       <TouchableOpacity onPress={() => navigation.navigate('InformationLimao')}>
         <Image
           style={styles.image}
@@ -142,17 +166,23 @@ export default function Drinks({ navigation }) {
         />
         </TouchableOpacity>
         <Text style={styles.texto}>Caipirinha de limão</Text>
+        </View>
       </View>
 
-      <View style={styles.column}>
-      <TouchableOpacity onPress={() => navigation.navigate('InformationMelancia')}>
-        <Image
-          style={styles.image}
-          source={require('../../asset/Drinks/twits.jpg')}
-        />
-        </TouchableOpacity>
-        <Text style={styles.texto}>Twist</Text>
+      <View style={styles.box}>
+        <View style={styles.inner}>
+          <TouchableOpacity onPress={() => navigation.navigate('InformationMelancia')}>
+            <Image
+              style={styles.image}
+              source={require('../../asset/Drinks/twits.jpg')}
+            />
+            </TouchableOpacity>
+            <Text style={styles.texto}>Twist</Text>
+        </View>
       </View>
+
+      </View>
+    </View>
     
     </ScrollView>
 
@@ -178,4 +208,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     top: -10
   },
+  box:{
+   width: '50%',
+   height: '50%',
+   padding: 5,
+  },
+  boxContainer:{
+    width: '100%',
+    height: '85%',
+    padding: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  inner:{
+    flex: 1,
+    alignItems: 'center'
+  }
 })
